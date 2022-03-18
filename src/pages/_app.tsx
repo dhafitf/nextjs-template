dc36a1c57fd2edd0d3e96c832f61f8b30cef6fa2
Dhafit Farenza <dhafidfarenza@gmail.com>
2022-03-18 22:48:10 +0700

    add loading bar animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,49 @@
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import Loading from "@components/layout/loading";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
 
-export default MyApp
+  const [state, setState] = useState({
+    isRouteChanging: false,
+    loadingKey: 0,
+  });
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: true,
+        loadingKey: prevState.loadingKey ^ 1,
+      }));
+    };
+
+    const handleRouteChangeEnd = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: false,
+      }));
+    };
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeEnd);
+    router.events.on("routeChangeError", handleRouteChangeEnd);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeEnd);
+      router.events.off("routeChangeError", handleRouteChangeEnd);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Loading isRouteChanging={state.isRouteChanging} key={state.loadingKey} />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
